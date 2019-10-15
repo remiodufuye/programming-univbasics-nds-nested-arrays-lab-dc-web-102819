ce1862b8fb7421f438f8f4560df65f4f2e43eadf
remi.odufuye <remi.odufuye@gmail.com>
2019-10-15 22:57:17 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,5 +1,7 @@
 # Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
 
+SORTED_ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+
 ENNET_HOUSE = [
   "Don Gately",
   "Joelle van Dyne",
@@ -19,19 +21,42 @@ ENFIELD_TENNIS_ACADEMY = [
 def assembled_matrix
   # Build an array that contains (or, "nests") the residents of The Ennet House
   # and the Enfield Tennis Academy as provided by the constants
+  
+    total_residents = [ENNET_HOUSE,ENFIELD_TENNIS_ACADEMY]
+                        
+    p total_residents.sort
+  
 end
 
 def array_literal_matrix
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
+  
+     total_residents = [ENNET_HOUSE.sort ,ENFIELD_TENNIS_ACADEMY.sort]
+     
+    p total_residents
+    
+    # myArray.sort { |x,y| x[0] <=> y[0] }
+    # string_array.sort_by { |string| string.length }
+    #  p total_residents.sort{|x,y| x[0] <=> y[0] }
+ 
+    
+  
 end
 
 def matrix_lookup(matrix, row, column)
   # Return the matrix's content at row and column
+  
+  p matrix[row][column] 
+  
 end
 
 def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at row and column to have the value of new_value
-  # Return the updated matrix
+  # Return the updated matrix 
+  
+  matrix[row][column] = new_value 
+  p matrix  
+  
 end
